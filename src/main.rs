1ceb42e70f2b81abfd532f2f6f43c2f55a053042
Nathan O'Donoghue <nodonoghue@sightlinepayments.com>
2023-02-28 10:02:43 -0800

    Logic flow updates

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,23 @@ fn main() {
 }
 
 fn input_player_count() -> i32 {
-    let mut num_players = String::new();
-    io::stdin()
-        .read_line(&mut num_players)
-        .expect("Please input a number of players.");
+    let mut num_players: String = String::new();
+    num_players = get_input();
+
+    //check that there is an input, check that the input is numeric
+
     num_players
         .trim()
         .parse()
         .expect("Number of players should be a number")
 }
+
+fn get_input() -> String {
+    let mut num_players = String::new();
+    io::stdin()
+        .read_line(&mut num_players)
+        .expect("Please input a number of players.");
+    num_players
+}
+
+// Need logic to check the inputs, display an error message in the terminal and ask for the input again

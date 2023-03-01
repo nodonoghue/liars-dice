b846eba3e60fe5a8a0aac960b46c655b604359cf
Nathan O'Donoghue <nodonoghue@sightlinepayments.com>
2023-02-28 16:44:58 -0800

    Added loop and validation to input.

--- src/main.rs
+++ src/main.rs
@@ -9,23 +9,29 @@ fn main() {
 }
 
 fn input_player_count() -> i32 {
-    let mut num_players: String = String::new();
-    num_players = get_input();
+    loop {
+        let num_players: String = get_input();
 
-    //check that there is an input, check that the input is numeric
-
-    num_players
-        .trim()
-        .parse()
-        .expect("Number of players should be a number")
+        //check that there is an input, check that the input is numeric
+        if check_input(num_players.clone()) {
+            break num_players.trim().parse().expect("Error");
+        } else {
+            println!("Error reading input, please enter the number of players as a number between 2 and 6.");
+        }
+    }
 }
 
 fn get_input() -> String {
     let mut num_players = String::new();
-    io::stdin()
-        .read_line(&mut num_players)
-        .expect("Please input a number of players.");
+    io::stdin().read_line(&mut num_players).expect("Error.");
     num_players
 }
 
-// Need logic to check the inputs, display an error message in the terminal and ask for the input again
+fn check_input(input: String) -> bool {
+    if input.trim().parse::<i32>().is_ok() {
+        let num = input.trim().parse::<i32>().expect("Error.");
+        (2..=6).contains(&num)
+    } else {
+        false
+    }
+}
